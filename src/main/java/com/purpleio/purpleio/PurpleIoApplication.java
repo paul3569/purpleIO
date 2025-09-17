b2a4633c2bd79e35327b882ced59de64bcae5211
DAME <lklkklkl@naver.com>
2025-09-17 15:59:27 +0900

    Initial commit

---
+++ src/main/java/com/purpleio/purpleio/PurpleIoApplication.java
@@ -0,0 +1,13 @@
+package com.purpleio.purpleio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurpleIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurpleIoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/purpleio/purpleio/PurpleIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purpleio.purpleio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurpleIoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
